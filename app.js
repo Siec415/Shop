9c1a6af27e6b729bafdaf7ec3d53feef579f310a
Anthony Lu <33469307+LiquidWat3r@users.noreply.github.com>
2019-12-07 00:59:03 -0800

    added more content

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+  $(".burger").click(function()
+        {
+            $("#side-tabs").css("width", "200px");
+            $("#row").css("margin-left", "200px");
+     
+            //$("#side-tabs").css("width", "0px");
+            //$("#row").css("margin-left", "0px");
+        });
+   
+
+});
